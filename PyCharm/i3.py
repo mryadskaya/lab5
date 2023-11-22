34d28241414d0bb11b25150dd9c4fb83ff9c182c
mryadskaya <125180218+mryadskaya@users.noreply.github.com>
2023-11-22 09:59:22 +0300

    Update i3.py

--- PyCharm/i3.py
+++ PyCharm/i3.py
@@ -1,10 +1,8 @@
-#!/usr/bin/env python3
+# !/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
+
 if __name__ == '__main__':
-    n = int(input("Введите значение n: "))
-    k = int(input("Введите значение k: "))
-    total_sum = 0
-    for i in range(10 ** (n - 1), 10 ** n):
-        if i % k == 0:
-            total_sum += i
-    print("Сумма всех", n, "-значных чисел, кратных", k, ":", total_sum)
+    n, k = map(int, input().split())
+    a, b = (10 ** n + k - 1) // k, (10 ** (n + 1) - 1) // k
+    print((b - a + 1) * k * (a + b) // 2)
